d80c1191f6df5dd423a3758f96fa11f2d48286f9
Ahmed Gharseldin <A.Ghars@outlook.com>
2021-02-22 03:36:41 +0400

    debug

--- Data Structures/DoubleLinkedList.hpp
+++ Data Structures/DoubleLinkedList.hpp
@@ -12,77 +12,208 @@
 #include "Node.hpp"
 
 template <typename E>
-class DoubleLinkedList {
+class DoubleLinkedList
+{
 
 private:
     Node<E> *head = NULL;
     Node<E> *tail = NULL;
-    int count;
-    
+    int count = 0;
+    //....DID NOT WORK....///
+//    Node<E> findNode(int index)
+//    {
+//        Node<E> *current = head;
+//        for (int i = 0; current != NULL; i++)
+//        {
+//            if (i == index)
+//                break;
+//            else
+//                current = current->next;
+//        }
+//        return current;
+//    };
+
 public:
-    DoubleLinkedList(){
-        
+    DoubleLinkedList()
+    {
+        Node<E> *temp = new Node<E>();
+        temp->next = NULL;
+        temp->prev = NULL;
+        head = tail = NULL;
     };
-    
-    E front(){
 
+    E front()
+    {
+        assert(head != NULL);
+        return head->data;
     };
-    
-    E back(){
 
+    E back()
+    {
+        assert(tail != NULL);
+        return tail->data;
     };
-    
-    void addFront(E e){
 
+    void addFront(E e)
+    {
+        if (head)
+        {
+            Node<E> *temp = new Node<E>();
+            temp->next = head;
+            temp->prev = NULL;
+            temp = head->prev;
+            head = temp;
+            count++;
+        }
+        else //first node
+        {
+            Node<E> *temp = new Node<E>();
+            temp->data = e;
+            temp->next = NULL;
+            temp->prev = NULL;
+            head = tail = temp;
+        }
     };
-    
-    void addBack(E e){
 
+    void addBack(E e)
+    {
+        if (head)
+        {
+            Node<E> *temp = new Node<E>();
+            temp->data = e;
+            temp->next = NULL;
+            temp->prev = tail;
+            tail->next = temp;
+            tail = temp;
+            count++;
+        }
+        else
+        {
+            addFront(e);
+        }
     };
-    
-    void removeFront(){
 
+    void removeFront()
+    {
+        assert(count > 0);
+        head = head->next;
+        head->prev = NULL;
+        count--;
+        Node<E> *current = head;
     };
-    
-    void removeBack(){
 
+    void removeBack()
+    {
+        assert(count > 0);
+        tail = tail->prev;
+        tail->next = NULL;
+        count--;
     };
-    
+
     void swap(int firstPosition, int secondPosition){
-
+        assert((firstPosition >= 0 && firstPosition <= count) && (secondPosition >= 0 && secondPosition <= count));
+        Node<E> *firstNode = head;
+        for (int i = 0; firstNode != NULL; i++)
+        {
+            if (i == firstPosition)
+                break;
+            else
+                firstNode = firstNode->next;
+        }
+        Node<E> *secondNode = firstNode->next;
+        firstNode->next = secondNode->next;
+        secondNode->prev = firstNode->prev;
+        firstNode->prev = secondNode;
+        secondNode->next = firstNode;
     };
-    
-    int size(){
-
-    };
-    
-    bool empty(){
-
-    };
-    
-    void clear(){
-
+ 
+    int size()
+    {
+        return count;
     };
 
-    E at(int position){
+    bool empty()
+    {
+        return (!count);
+    };
 
+    void clear()
+    {
+        head->next = NULL;
+        tail->prev = NULL;
+        head = tail = NULL;
+        count = 0;
+    };
+
+    E at(int position)
+    {
+        assert(position >= 0 && position <= count);
+        Node<E> *current = head;
+        for (int i = 0; current != NULL; i++)
+        {
+            if (i == position)
+                break;
+            else
+                current = current->next;
+        }
+        return current->data;
     }
-    
-    E operator[](const int index){
 
+    E operator[](const int index)
+    {
+        return at(index);
     }
-    
+
     // Add an element after a certain node
-    void addAt(int position, E e){
-
+    void addAt(int position, E e)
+    {
+        assert(position >= 0 && position <= count);
+        if (position == count)
+            addBack(e);
+        else
+        {
+            Node<E> *previousNode = head;
+            for (int i = 0; previousNode != NULL; i++)
+            {
+                if (i == position)
+                    break;
+                else
+                    previousNode = previousNode->next;
+            }
+            Node<E> *insertNode = new Node<E>();
+            insertNode->data = e;
+            insertNode->next = previousNode->next;
+            insertNode->prev = previousNode;
+            previousNode->next = insertNode;
+            previousNode->next->prev = insertNode;
+        }
     };
-    
-    // Remove a sepcific node
-    void remove(int index){
-        
+
+    // Remove a specific node
+    void remove(int index)
+    {
+        assert(index > 0 && index <= count);
+        if (index == 0)
+            removeBack();
+        else if (index == count)
+            removeFront();
+        else
+        {
+            Node<E> *deleteNode = head;
+            for (int i = 0; deleteNode != NULL; i++)
+            {
+                if (i == index)
+                    break;
+                else
+                    deleteNode = deleteNode->next;
+            }
+            deleteNode->prev = deleteNode->next;
+            deleteNode->next = deleteNode->prev;
+            delete deleteNode;
+        }
     };
 
-    void printContents(){        
+    void printContents(){
         Node<E> *cursor = head;
         cout<<"Forward connections - ";
         cout<<"[Head] => ";
